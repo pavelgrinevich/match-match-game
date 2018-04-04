991d4f79856ed5606dfd7269aee9a5cbabf463e7
Pavel Grinevich <grinevich.ps@gmail.com>
2018-04-04 14:23:29 +0300

    Prepare project

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HtmtlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const extractCSS = new ExtractTextPlugin('./styles/style.css');
+
+module.exports = {
+  mode: 'none',
+  devtool: 'source-map',
+  entry: path.join(__dirname, './source/index.js'),
+  output: {
+    filename: './js/match-match-game.js',
+    path: path.join(__dirname, 'build')
+  },
+  plugins: [
+		new CleanWebpackPlugin(['build']),
+		new HtmtlWebpackPlugin({
+			template: './source/index.html'
+    }),
+    extractCSS
+	],
+	module: {
+		rules: [
+			{
+        test: /\.css$/,
+        use: extractCSS.extract([ 
+          'css-loader' 
+        ])
+      }
+		]
+	}
+};
