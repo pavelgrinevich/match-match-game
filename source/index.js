ba692e8829d62329889f000482e625a64ac5cea7
Pavel Grinevich <grinevich.ps@gmail.com>
2018-04-05 21:24:43 +0300

    Add the playing field

--- source/index.js
+++ source/index.js
@@ -1,17 +1,25 @@
 import './styles/style.css';
-import img from './images/1.jpg';
-import './images/2.jpg';
-import './images/3.jpg';
-import './images/4.jpg';
-import './images/5.jpg';
-import './images/6.jpg';
-import './images/7.jpg';
-import './images/8.jpg';
-import './images/9.jpg';
-import './images/10.jpg';
-import './images/11.jpg';
-import './images/12.jpg';
-import './images/card-back.jpg';
+import img1 from './images/1.jpg';
+import img2 from './images/2.jpg';
+import img3 from './images/3.jpg';
+import img4 from './images/4.jpg';
+import img5 from './images/5.jpg';
+import img6 from './images/6.jpg';
+import img7 from './images/7.jpg';
+import img8 from './images/8.jpg';
+import img9 from './images/9.jpg';
+import img10 from './images/10.jpg';
+import img11 from './images/11.jpg';
+import img12 from './images/12.jpg';
+import imgCardBack from './images/card-back.jpg';
 
-const body = document.getElementsByTagName('body')[0]
-body.innerHTML = `<img src="${img}" alt="card">`;
+const cardsArray = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12];
+const playingField = document.getElementsByClassName('playing-field')[0]
+
+/*for (let i = 0, l = cardsArray.length; i < l; i++) {
+    const div = document.createElement('div');
+    div.innerHTML = `<img src="${cardsArray[i]}" alt="card">`;
+    playingField.appendChild(div);
+}*/
+
+ 
