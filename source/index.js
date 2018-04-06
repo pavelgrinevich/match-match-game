8ef314f86d63769054bf5251c8ad8bd0ffd3d87d
Pavel Grinevich <grinevich.ps@gmail.com>
2018-04-06 23:21:05 +0300

    Add design of cards and controls

--- source/index.js
+++ source/index.js
@@ -1,4 +1,5 @@
 import './styles/style.css';
+
 import img1 from './images/1.jpg';
 import img2 from './images/2.jpg';
 import img3 from './images/3.jpg';
@@ -11,15 +12,40 @@ import img9 from './images/9.jpg';
 import img10 from './images/10.jpg';
 import img11 from './images/11.jpg';
 import img12 from './images/12.jpg';
-import imgCardBack from './images/card-back.jpg';
+import imgCardBack1 from './images/card-back1.jpg';
+import imgCardBack2 from './images/card-back2.jpg';
+import imgCardBack3 from './images/card-back3.jpg';
+import imgCardBack4 from './images/card-back4.jpg';
+import imgCardBack5 from './images/card-back5.jpg';
 
 const cardsArray = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12];
-const playingField = document.getElementsByClassName('playing-field')[0]
+const cardBackArray = [imgCardBack1, imgCardBack2, imgCardBack3, imgCardBack4, imgCardBack5];
+const playingField = document.getElementsByClassName('playing-field')[0];
 
-/*for (let i = 0, l = cardsArray.length; i < l; i++) {
+for (let i = 0, l = cardsArray.length; i < l; i++) {
     const div = document.createElement('div');
-    div.innerHTML = `<img src="${cardsArray[i]}" alt="card">`;
+    div.className = 'card';
+    div.style.backgroundImage = `url(${cardsArray[i]})`;
     playingField.appendChild(div);
-}*/
+}
 
+for (let i = 0, l = cardsArray.length; i < l; i++) {
+    const div = document.createElement('div');
+    div.className = 'card back';
+    let cardBack = Math.floor(Math.random() * cardBackArray.length);
+    console.log(cardBack);
+    div.style.backgroundImage = `url(${cardBackArray[cardBack]})`;
+    playingField.appendChild(div); 
+}
+
+//menu.addEventListener('click', function() {alert('меню')});
+
+//reload.addEventListener('click', function() {alert('релоад')});
  
+/*const width = document.body.clientWidth;  
+const height = document.body.clientHeight; 
+
+const matchMatchGame = document.getElementsByClassName('match-match-game')[0];*/
+
+/*matchMatchGame.style.width = width + 'px';
+matchMatchGame.style.height = height + 'px';*/
